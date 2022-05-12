a660f860138902d16db5353dd0280714f384f646
Abdulrahman Seliem <oldtimeprogrammer@yahoo.com>
2022-05-12 08:28:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using WebApi.Authorization;
+using WebApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// add services to DI container
+{
+    var services = builder.Services;
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen();
+    services.AddCors();
+    services.AddControllers();
+
+    // configure DI for application services
+    services.AddScoped<IUserService, UserService>();
+}
+
+var app = builder.Build();
+
+// configure HTTP request pipeline
+{
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+    
+    // global cors policy
+    app.UseCors(x => x
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+
+    // custom basic auth middleware
+    app.UseMiddleware<BasicAuthMiddleware>();
+
+    app.MapControllers();
+}
+
+app.Run();
